9dd16b5954e355a37779b9fb88f0faa659bc0606
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-06-14 20:18:44 +0400

    Updates

---
+++ precache-manifest.78740c718e32d079bbbab406e70c7ba2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568cd1facc7d4b013543e69b49ce9904",
+    "url": "/shortly-react/index.html"
+  },
+  {
+    "revision": "117bb2322defa70d9b85",
+    "url": "/shortly-react/static/css/main.df44e3ec.chunk.css"
+  },
+  {
+    "revision": "f2a795ef8c0c0a815b64",
+    "url": "/shortly-react/static/js/2.a0b930c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shortly-react/static/js/2.a0b930c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117bb2322defa70d9b85",
+    "url": "/shortly-react/static/js/main.8482e5e0.chunk.js"
+  },
+  {
+    "revision": "40ff9a3d488dfbf6c08c",
+    "url": "/shortly-react/static/js/runtime-main.517e514f.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/shortly-react/static/media/boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/shortly-react/static/media/brand.1922b68a.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/shortly-react/static/media/customizable.cc099cea.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/shortly-react/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/shortly-react/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/shortly-react/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/shortly-react/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/shortly-react/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/shortly-react/static/media/logo.6387840f.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/shortly-react/static/media/records.e3d9caa9.svg"
+  }
+]);
